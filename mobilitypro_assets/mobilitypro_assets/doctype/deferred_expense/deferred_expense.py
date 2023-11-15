d9da34bbfeaa0e40ee65f41b18728cf32df0afc6
ahmed zaytoon <citybirdman2@gmail.com>
2023-11-15 10:51:20 +0000

    the final first release

--- mobilitypro_assets/mobilitypro_assets/doctype/deferred_expense/deferred_expense.py
+++ mobilitypro_assets/mobilitypro_assets/doctype/deferred_expense/deferred_expense.py
@@ -5,7 +5,7 @@
 import frappe
 from frappe import _
 from frappe.model.delete_doc import update_flags
-from frappe.utils import (add_months, date_diff, get_last_day, get_first_day, getdate)
+from frappe.utils import (add_months, date_diff, get_last_day, get_first_day, add_days, getdate)
 from mobilitypro_assets.tasks import update_balance
 
 from erpnext.controllers.accounts_controller import AccountsController
@@ -16,7 +16,7 @@ class DeferredExpense(AccountsController):
 		self.flags.ignore_links = True
 
 	def before_save(self):
-		self.calculate_adjustment_entries()
+		self.make_adjustment_entries()
 		update_balance(self)
 
 	def on_submit(self):
@@ -31,60 +31,7 @@ class DeferredExpense(AccountsController):
 
 	def validate_adjustment_entries(self):
 		if self.total_number_of_adjustments.is_dirty():
-			self.calculate_adjustment_entries()
-
-
-	def calculate_adjustment_entries(self):
-		if(self.validate_basics()):
-			return
-			
-		expense_amount = self.deduct_opening()
-		number_of_adjustments = self.total_number_of_adjustments
-		#	clearing the table
-		self.set("schedules", [])
-		#	validating adjustments and date
-		accumulated_amount, last_day, check_month = (self.opening_realized_expense_balance or 0, "", 0)
-		# frappe.throw(frappe.utils.cstr(get_first_day(self.start_service_date) == getdate(self.start_service_date)))
-		if get_first_day(self.start_service_date) == getdate(self.start_service_date):
-			last_day = get_last_day(self.start_service_date)
-		elif get_last_day(self.start_service_date) == getdate(self.start_service_date):
-			last_day = add_months(get_last_day(self.start_service_date), 1)
-		else:
-			if not self.number_of_adjustments_booked:
-				last_day = get_last_day(self.start_service_date)
-				check_month = 1
-			else:
-				last_day = add_months(get_last_day(self.start_service_date), self.number_of_adjustments_booked)
-		monthly_amount = self.gross_expense_amount / number_of_adjustments
-		entries_number = number_of_adjustments + check_month
-		if self.number_of_adjustments_booked and self.is_existing_expense:
-			entries_number -= self.number_of_adjustments_booked - 1
-
-		for n in range(entries_number):
-			#	checking the start service date
-			if n != 0:
-				last_day = get_last_day(add_months(last_day, 1))
-			amount = monthly_amount
-			if(n == 0 and check_month == 1):
-				month_range = date_diff(last_day, get_first_day(self.start_service_date)) + 1
-				amount = monthly_amount * date_diff(last_day, self.start_service_date)/month_range
-
-			elif n is (entries_number - 1):
-				amount = self.gross_expense_amount - accumulated_amount
-			if self.gross_expense_amount == accumulated_amount:
-				break
-			accumulated_amount += amount
-			self.append(
-				"schedules",
-				{
-					"schedule_date": last_day,
-					"adjustment_amount": amount,
-					"accumulated_adjustment_amount": accumulated_amount,
-				},
-			)
-		if(check_month):
-			self.edit_last_adjustment_last_date(months= self.total_number_of_adjustments + check_month -1)
-			
+			self.calculate_adjustment_entries()	
 
 	def validate_basics(self):
 		# checking if all required fields are filled
@@ -93,25 +40,6 @@ class DeferredExpense(AccountsController):
 		
 		if self.gross_expense_amount == 0:
 			frappe.throw("The expense amount cannot be zero")
-
-
-	def deduct_opening(self):
-		expense_amount, number_of_adjustments = (self.gross_expense_amount, self.total_number_of_adjustments)
-		if self.is_existing_expense:	
-			if self.opening_realized_expense_balance and self.opening_realized_expense_balance != 0:
-
-				if not self.number_of_adjustments_booked:
-					frappe.throw(_("Please add Number Of Adjustments Booked"))
-
-				if self.opening_realized_expense_balance == expense_amount:
-					frappe.throw(_("Opening Realized Expense Balance must be less than Gross Expense Amount"))
-
-				#	calculating the remaining balance and number of adjustments
-				expense_amount -= self.opening_realized_expense_balance
-			else:
-				frappe.throw("For existing expense Opening Realized Expense Balance cannot be Zero")
-
-		return expense_amount
 
 	def edit_last_adjustment_last_date(self, date = None, months = 0):
 		if not date:
@@ -132,6 +60,81 @@ class DeferredExpense(AccountsController):
 			je.cancel()
 			je.delete(force=True, ignore_permissions=True, delete_permanently=True)
 
-
 	def set_status(self, status):
 		self.db_set("status", status)
+
+
+
+
+
+
+
+
+
+	######################################################################################################################
+	def make_adjustment_entries(self):
+		starting_date , num_of_months, expense_amount = self.start_service_date, self.total_number_of_adjustments, self.gross_expense_amount
+		if self.validate_is_existing_expense():
+			starting_date = add_months(starting_date, self.number_of_adjustments_booked)
+			expense_amount -= self.opening_realized_expense_balance
+			num_of_months -= self.number_of_adjustments_booked
+		schedules = self.get_adjustment_entries(starting_date, num_of_months, expense_amount, self.validate_is_existing_expense())
+		self.set("schedules", schedules)
+
+
+
+
+	def validate_is_existing_expense(self):
+		if self.is_existing_expense:
+			if self.opening_realized_expense_balance and self.opening_realized_expense_balance != 0:
+				if not self.number_of_adjustments_booked:
+					frappe.throw(_("Please add Number Of Adjustments Booked"))
+
+				if self.opening_realized_expense_balance >= self.gross_expense_amount:
+					frappe.throw(_("Opening Realized Expense Balance must be less than Gross Expense Amount"))
+
+				if self.opening_realized_expense_balance == 0:
+					frappe.throw(_("Opening Realized Expense Balance cannot be zero"))
+
+				return True
+		else: return False
+
+
+	def get_adjustment_entries(self, starting_date, num_of_months, expense_amount, deducted = False):
+		num_of_entries = num_of_months + 1 if (getdate(starting_date) != get_first_day(starting_date) 
+						 and getdate(starting_date) != get_last_day(starting_date)) else num_of_months
+		date = starting_date
+		accumulated_amount, schedules = 0, []
+		for n in range(num_of_entries):
+			amount, entry_day = 0, get_last_day(add_months(date, n))
+			if n == 0 and num_of_entries != num_of_months and not deducted:
+				part = expense_amount / num_of_months
+				month_days = date_diff(get_last_day(entry_day), get_last_day(add_months(entry_day, -1)))
+				amount_per_day = part / month_days
+				first_month_remaining_days = date_diff(get_last_day(date), date)
+				amount = first_month_remaining_days * amount_per_day
+
+			elif n+1 == num_of_entries:
+				entry_day = add_days(add_months(starting_date, num_of_months), -1)
+				amount = expense_amount - accumulated_amount
+				
+			else:
+				if num_of_entries != num_of_months and not deducted:
+					part = expense_amount / num_of_months
+					amount = (expense_amount - part) / (num_of_entries - 2)
+				elif deducted:
+					amount = self.gross_expense_amount / self.total_number_of_adjustments
+					if self.opening_realized_expense_balance / self.number_of_adjustments_booked > amount:
+						frappe.throw(_("The calculation of Opening Realized Expense Balance over Number Of Adjustments Booked cannot be greater than the Adjustment Amount per month (" + str(amount) + ")"))
+
+				else:
+					amount = expense_amount / num_of_entries
+
+			accumulated_amount += amount
+			if amount != 0:
+				schedules.append({
+					"schedule_date": entry_day,
+					"adjustment_amount": amount,
+					"accumulated_adjustment_amount": accumulated_amount
+					})
+		return schedules
